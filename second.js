553f5a1010924ac373c0983295f7189300d6b44c
Saka <pandyasakaaji@gmail.com>
2026-07-11 23:52:16 +0700

    remove second.js

--- second.js
+++
@@ -1,9 +0,0 @@
-const title = document.createElement("h1")
-const text = document.createElement("p")
-
-title.textContent = 'Hello World!'
-text.textContent = 'This is a paragraph.'
-
-const nodeArray = [title, text]
-
-nodeArray.forEach()
