cf24cefae083c3e661a73337c27d297a159c6fa9
Saka <pandyasakaaji@gmail.com>
2026-07-12 22:05:40 +0700

    add log for better visibility

--- js/reduce.js
+++ js/reduce.js
@@ -1,4 +1,10 @@
 let arr = [1, 2, 3, 5, 7]
 
-let result = arr.reduce((a, b) => a + b, 0)
-console.log(result)
+let result = arr.reduce((a, b) => {
+  console.log(`total now: ${a}`)
+  console.log(`added: ${b}`)
+  console.log(" - ")
+  return a + b
+}, 0)
+
+console.log(`the result is ${result}`)
