ce35dd8d199d043099828b23666e57173a74bf3c
Saka <pandyasakaaji@gmail.com>
2026-07-20 22:53:12 +0700

    LeetCode 2620. Counter

---
+++ js/2620.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+
+var createCounter = function (n) {
+  return function () {
+    n++
+    return n - 1
+  };
+};
+
+counter = createCounter(10)
+counter()
+counter()
+counter()
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
