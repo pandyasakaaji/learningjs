9661e59227680f8121f98500d77372724c811001
Saka <pandyasakaaji@gmail.com>
2026-07-21 22:23:37 +0700

    LeetCode 1260. Shift 2D Grid (on progress)

---
+++ js/LeetCode/1260.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number[][]}
+ */
+
+grid = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+// k = 1
+
+var shiftGrid = function (grid, k) {
+
+};
+
+for (i = 0; i < grid.length; i++) {
+  for (j = 0; j < grid[i].length; j++) {
+    // cek j tok
+    if (j == (grid[i].length - 1)) {
+      // cek i karo j
+      if (i == (grid.length - 1) && (j == grid[i].length - 1)) {
+        console.log("i dan j")
+
+        continue
+      }
+
+      console.log("j tok")
+      // value kabeh yang sebaris
+    } else {
+      console.log("value roto")
+    }
+  }
+}
+
+// rumus untuk value yang rata
+// grid[i][j] >> grid[i][j+1] 
+grid[0][2] = grid[0][1]
+grid[0][1] = grid[0][0]
+
+// rumus untuk value yang ganti baris yang selanjutnya
+// Element at grid[i][n - 1] moves to grid[i + 1][0].
+grid.at(0).at(-1) = grid[1][0]
+
+// rumus untuk value paling belakang agar kedepan
+// Element at grid[m - 1][n - 1] moves to grid[0][0].
+grid[0][0] = grid.at(-1).at(-1)
+
+
+console.log(grid)
